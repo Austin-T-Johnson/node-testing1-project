6e0c95cb1fb0ca918db41628761662776092ae1c
Austin Johnson <austintjohnson92@gmail.com>
2022-06-23 14:24:45 -0700

    wip

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ class Counter {
      * @param {number} initialNumber - the initial state of the count
      */
     constructor(initialNumber) {
-        // ✨ initialize whatever properties are needed
+       this.count = initialNumber
     }
 
     /**
@@ -70,7 +70,7 @@ class Counter {
      * counter.countDown() // returns 0
      */
     countDown() {
-        // ✨ implement
+     return this.count > 0 ? this.count-- : 0
     }
 }
 
